6615445429af26269bed802f09611d6e5c4c86de
omine emi <rep.pro.o.e@gmail.com>
2025-09-16 01:16:52 +0900

    Set up controllers, routes, for page displays

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -1,10 +1,16 @@
 <!-- resources/views/register.blade.php -->
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
+
 @section('content')
     <h1>【仮】ユーザーログインページ</h1>
 
     <!-- TODO: フォーム本体の作成 -->
+    <form class="register-form" action="" method="post">
+    @csrf
     <!-- TODO: 入力欄スタイル調整 -->
     <!-- TODO: ボタンデザイン調整 -->
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ConfirmController;
+use App\Http\Controllers\ThanksController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +24,8 @@ use App\Http\Controllers\RegisterController;
 // });
 
 Route::get('/register', [RegisterController::class, 'register']);
+Route::get('/login', [LoginController::class, 'login']);
+Route::get('/contact', [ContactController::class, 'contact']);
+Route::get('/confirm', [ConfirmController::class, 'confirm']);
+Route::get('/thanks', [ThanksController::class, 'thanks']);
+Route::get('/admin', [AdminController::class, 'admin']);
