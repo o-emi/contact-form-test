5e1fc6061751723ad7179d6df4fbe41504f0191c
omine emi <rep.pro.o.e@gmail.com>
2025-09-20 11:57:07 +0900

    feature/add-login完了

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -6,8 +6,24 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function login()
+    // GET（表示）用
+    public function showLoginForm()
     {
         return view('login');
     }
+
+// POST用
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            return redirect()->intended('/'); // ログイン成功後の遷移先
+        }
+
+        return back()->withErrors([
+            'email' => '認証に失敗しました。',
+        ]);
+    }
 }
+
