c6b8ab18cdf7fce4d68a4c47d2a4ad07c4747841
omine emi <rep.pro.o.e@gmail.com>
2025-09-21 12:48:45 +0900

    Admi-form途中

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -8,12 +8,45 @@
 @endsection
 
 @section('content')
-<!-- 動作確認 -->
-<h2>管理画面</h2>
-<p>管理者専用ページです</p>
-<form method="POST" action="{{ route('logout') }}">
-        @csrf
+<div class="admin__content">
+
+    <div class="search__title">
+        <h2>Admin</h2>
+    </div>
+    <form clacc="search-form" action="{{ route('logout') }}" method="POST">
+    @csrf
+<!-- search入力フォーム、後で修正あり -->
+    <div class="search-form__item">
+        <input class="search-form__item-input" type="text"
+        name="" placeholder="名前やメールアドレスを入力してください" value="{{ old('') }}"/>
+<!-- 性別選択 -->
+        <select class="search-form__item-select" name="gender">
+                <option value="">性別</option>
+        </select>
+        <select class="search-form__item-select" name="categeory">
+                <option value="">お問い合わせの種類</option>
+        </select>
+        <select class="search-form__item-select" name="date">
+                <option value="">年/月/日</option>
+        </select>
+    </div>
+<!-- 検索ボタン -->
+
+    <div class="search-form__button">
+        <button class="search-form__button-submit" type="submit">検索</button>
+    </div>
+<!-- リセットフォーム -->
+    <div class="search-form__reset-button">
+        <button class="search-form__reset-button-submit" type="submit">リセット</button>
+    </div>
+<!-- エクスポート -->
+    <div class="export-form__button">
+        <button class="search-form__reset-button-submit" type="submit">export</button>
+    </div>
+
+
+
 
 </form>
-
+</div>
 @endsection
